565fe0d9ea7c3d659eccf0a4ca7c68c16af7f237
pauldoelle <57750270+pauldoelle@users.noreply.github.com>
2019-11-14 11:21:44 +0100

    Update section.js

--- js/section.js
+++ js/section.js
@@ -5,21 +5,23 @@ var t = TrelloPowerUp.iframe();
 // you can access arguments passed to your iframe like so
 var arg = t.arg('arg');
 
-t.render(function(){
-  // make sure your rendering logic lives here, since we will
-  // recall this method as the user adds and removes attachments
-  // from your section
-  t.card('attachments')
-  .get('attachments')
-  .filter(function(attachment){
-    return attachment.url.indexOf('http://www.nps.gov/yell/') == 0;
-  })
-  .then(function(yellowstoneAttachments){
-    var urls = yellowstoneAttachments.map(function(a){ return a.url; });
-    document.getElementById('urls').textContent = urls.join(', ');
-  })
-  .then(function(){
-    return t.sizeTo('#content');
-  });
+t.render(function () {
+    // make sure your rendering logic lives here, since we will
+    // recall this method as the user adds and removes attachments
+    // from your section
+    t.card('attachments')
+        .get('attachments')
+        .filter(function (attachment) {
+            return attachment.url.indexOf('https://www.youtube.com') == 0;
+        })
+        .then(function (youtubeAttachments) {
+            var urls = youtubeAttachments.map(function (a) {
+                return '<div class="video"><iframe src="' + a.url + '?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
+            });
+            document.getElementById('videos').innerHTML = urls.join(' ');
+        })
+        .then(function () {
+            return t.sizeTo('#content');
+        });
 });
 
